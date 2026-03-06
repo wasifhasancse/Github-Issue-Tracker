e0e97cab58ea66ff4fbb4e7df890a0c42128bd10
Wasif Hasan <wasifhasancse@gmail.com>
2026-03-07 00:06:46 +0600

    design the navigation bar

--- js/login.js
+++ js/login.js
@@ -6,8 +6,10 @@ const passwordInput = document.getElementById("input-password");
 
 document.getElementById("sign-in-button").addEventListener("click", function () {
   if (userNameInput.value === validUserName && passwordInput.value === validPassword) {
-    alert("Login successful!");
+    window.location.href = "dashboard.html";
   } else {
+    userNameInput.value = "";
+    passwordInput.value = "";
     document.getElementById("error-message").textContent = "Invalid credentials. Please try again.";
     document.getElementById("signInModal").showModal();
   }
